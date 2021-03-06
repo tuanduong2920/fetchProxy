c5f68f8cd480f9fc383a53a292532c3547ecce8f
TDuong <tuanduong2920@gmail.com>
2021-03-06 22:17:22 +0700

    init

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.port = process.env.PORT || 3000;
+exports.port = process.env.PORT || 3001;
 exports.enable_logging = false;
 exports.fetch_regex = /^\/fetch\/(.*)$/; // The URL to look for when parsing the request.
 exports.proxy_request_timeout_ms = 10000; // The lenght of time we'll wait for a proxy server to respond before timing out.
